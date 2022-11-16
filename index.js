b29d79e41a2af6c231d782dc121ca47b5edc4684
dramallama14 <116812565+dramallama14@users.noreply.github.com>
2022-11-16 22:43:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+// const score = 100
+// if (score == 100) {
+//     console. log("lulus");
+// } else if (score < 100) {
+//     console.log ("tidak lulus") ;
+// }
+
+
+// const tummy = "kerja"
+// switch(tummy) {
+//     case "lapar":
+//         console.log("makan");
+//         break;
+//     case "kosong":
+//         console.log("asam lambung naik");
+//         break;
+//     case "penuh":
+//         console.log("ngantuk");
+//         break
+//     default: 
+//         console.log("tidur")
+//         break
+// }
+
+
+// let button = false;
+
+// let render = !! button ? "sidebar tampil" : "sidebar hilang"
+
+// console.log (render);
+
+
+// for (let i = 0; i <=10; i++) {
+//     console.log (i);
+// }
+
+
+// for (let i = 5; i >=-5; i--) {
+//     console.log (i);
+// }
+
+// for (let i = 2; i <=20; i=i+2) {
+//     console.log (i);
+// }
+
+for (let i = 1; i <=15; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log ("fizzbuzz");
+    } else if (i % 3 == 0) {
+        console.log ("fizz");
+    } else if (i % 5 == 0) {
+        console.log("buzz");
+    } else {
+        console.log (1);
+    }
+}
+
+
